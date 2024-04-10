24e4a9a5d6b40316f36059c3684700e4a8a41439
asseternity <nakupovasset@gmail.com>
2024-04-10 14:44:46 +0500

    Fixing bug

--- src/api_requester.js
+++ src/api_requester.js
@@ -1,7 +1,7 @@
 export default class APIRequester {
   key = "d7d5bc3e633041a29e264636240704";
   async requestWeatherForecast(city) {
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=${this.key}&q=${city}&days=3`;
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${this.key}&q=${city}&days=3`;
     const response = await fetch(url, { mode: "cors" });
     return response;
   }
